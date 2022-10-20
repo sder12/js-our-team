d99be60c3539e8e29ad3025d09aa378a1629bd40
sder12 <sara.cetto@gmail.com>
2022-10-20 15:18:57 +0200

    Team Array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//[]ARRAY with OBJ TEAM members 
+//[*]ARRAY with OBJ TEAM members 
         //creare array team : nome ruolo foto
 //[]STAMPARE IN CONSOLE
         //ciclo for per percorrere array
@@ -9,3 +9,37 @@
         // img stamparare come scritta
 // Bonus 1: stampare l img come IMG (link)
 //Bonus 2: stampare info nelle card 
+
+const teamArray = [
+    {   //1. WAYNE
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "wayne-barnett-founder-ceo.jpg",
+    },
+    {   //2. ANGELA
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg",
+    },
+    {   //3. WALTER
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg",
+    },
+    {   //4. ANGELA
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg",
+    },
+    {   //5.SCOTT
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg",
+    },
+    {   //6. BARBARA
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg",
+    }
+]
+console.log(teamArray)
