5584bdf65e6e10dec2a711733f74153b27d8ae05
sder12 <sara.cetto@gmail.com>
2022-10-20 15:13:19 +0200

    Layout in Html + instructions in JS

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//[]ARRAY with OBJ TEAM members 
+        //creare array team : nome ruolo foto
+//[]STAMPARE IN CONSOLE
+        //ciclo for per percorrere array
+        // ciclo for in per percorrere l'interno di ogni oggetto
+        //stampare in console
+//[]STAMPARE nel DOM 
+        // stampare stesse info nel dom .innerHTML 
+        // img stamparare come scritta
+// Bonus 1: stampare l img come IMG (link)
+//Bonus 2: stampare info nelle card 
